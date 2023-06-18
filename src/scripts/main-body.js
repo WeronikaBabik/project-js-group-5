0b2030a6999a43db92c226b4112edc4aee478248
Piotr Bakuliński <pbakul1807@gmail.com>
2023-06-18 14:02:12 +0200

    alert1

--- src/scripts/main-body.js
+++ src/scripts/main-body.js
@@ -1,80 +1,20 @@
-import { getMovieTrailer, getBySearch, getInfoAboutMovie } from './api';
-import noimage from '../images/header-main/noimage.jpg';
+import { getMovieTrailer, getTrending } from './api';
 import { getGenre } from './genres';
 
 export const mainGallery = document.querySelector('.gallery');
-export const input = document.querySelector('.search-input');
-const modalCard = document.querySelector('.modal');
-const movie = document.querySelector('.movie');
-const searchForm = document.querySelector('.search-form');
-const modal = document.querySelector('.modal');
 
-searchForm.addEventListener('submit', showResultsOnSearch);
-
-export function addEventToCard(cards) {
-  cards.forEach(card => {
-    card.addEventListener('click', () => showPopUp(card));
-  });
-}
-export async function showPopUp(card) {
-  modalCard.classList.add('show-popup');
-  const movieId = card.getAttribute('data-movie');
-  const movie = await getInfoAboutMovie(movieId);
-  const movieTrailer = await getMovieTrailer(movieId);
-  const poster = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-    : noimage;
-  modal.innerHTML = `<span class="modal__closeBtn" type="button">x</span>
-  <div class="modal__info">
-    <div class="container__title">${movie.title}</h1>
-        <div class="info__movie">
-          <div class="info__name">
-            <p class="name">Vote / Votes</p>
-            <p class="name">Popularity</p>
-            <p class="name">Original Title</p>
-            <p class="name">Genre</p>
-          </div>
-          <div class="info__value">
-            <p class="value">
-              <span class="value">${
-                movie.vote_average
-              }</span>/<span class="value">${movie.vote_count}</span>
-            </p>
-            <p class="value">${movie.popularity}</p>
-            <p class="value">${movie.original_title}</p>
-            <p class="value">${getGenre(movieId)}</p>
-          </div>
-          <h2 class="about">About</h2>
-          <p class="about__text">
-            ${movie.overview}
-          </p>
-          <div class="trailer"><iframe width="500" height="300" src="https://www.youtube.com/embed/${movieTrailer}" title="Youtube"></iframe></div>
-          <ul class="button">
-            <li class="button__item">
-              <button class="button_watched" type="button">
-                ADD TO WATCHED
-              </button>
-            </li>
-            <li class="button__item">
-              <button class="button__queue" type="button">ADD TO QUEUE</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>`;
-  const modalCloseBtn = document.querySelector('.modal__closeBtn');
-  modalCloseBtn.addEventListener('click', () =>
-    modalCard.classList.remove('show-popup')
-  );
-}
-
-async function showResultsOnSearch(e) {
-  e.preventDefault();
-  const query = input.value;
-  const data = await getBySearch(query);
-  mainGallery.innerHTML = data
+getTrending().then(data => {
+  mainGallery.insertAdjacentHTML('beforeend', showGallery(data));
+});
+export function showGallery(movies) {
+  return movies
     .map(movie => {
+      //let movieId = movie.id;
+      //const keyOfTrailer = getMovieTrailer(movie.id).then(data => {
+      //  for (let value of data) {
+      //   console.log(value.key);
+      // }
+      //});
       let date = '';
       if (movie.release_date) {
         date = movie.release_date;
