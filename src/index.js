353c29a362b95c4600457500163c711bc867771c
Weronika Babik <weronika.babik@outlook.com>
2023-06-18 22:58:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-<<<<<<< Updated upstream
-import dataFromApi from './scripts/api';
-=======
 import './sass/main.scss';
 import * as genres from './scripts/genres';
+import * as genresList from './scripts/genres-list';
 import * as api from './scripts/api';
 import * as mainBody from './scripts/main-body';
-import * as search from './scripts/main-body-search';
-import dataFromApi from './scripts/api';
 import * as footerModal from './scripts/footer-modal';
 import * as switchBtn from './scripts/switchBtn';
-import * as modal from './scripts/modal';
 import * as scroll from './scripts/scrollButton';
+import * as aboutMovie from './scripts/get-trending';
 import * as pagination from './scripts/pagination';
 import * as paginationRefs from './scripts/pagination-refs';
->>>>>>> Stashed changes
