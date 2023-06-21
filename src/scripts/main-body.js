41ba102127555feea5f01d8477c6c1b068aca59b
VeranikaBabik <veranikababik@gmail.com>
2023-06-21 13:11:00 +0200

    markup

--- src/scripts/main-body.js
+++ src/scripts/main-body.js
@@ -1,8 +1,8 @@
 import { getMovieTrailer, getBySearch, getInfoAboutMovie } from './api';
 import { showGallery } from './get-trending';
 import Notiflix from 'notiflix';
-import { markupModal } from './markup-modal';
-import { markup } from './markup';
+import { getGenre } from './genres';
+import noimage from '../images/header-main/noimage.jpg';
 
 export const mainGallery = document.querySelector('.gallery');
 export const input = document.querySelector('.search-input');
@@ -24,7 +24,60 @@ export async function showPopUp(card) {
 
   const movie = await getInfoAboutMovie(movieId);
   const movieTrailer = await getMovieTrailer(movieId);
-  modal.innerHTML = markupModal(movie);
+  const genres = movie.genres.map(genre => genre.id);
+  const poster = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    : noimage;
+  modal.innerHTML = `
+  <div class="modal__info">
+  <span class="modal__closeBtn" type="button">x</span>
+  <div class="container__img">
+  
+  <button class="button_trailer" type="button" id="button_trailer"
+  ></button>
+    <img class="img" src="${poster}" alt="${movie.title}" />
+  </div>
+  <div class="container__text">
+    <div class="info__text">
+      <h1 class="info__title">${movie.title}</h1>
+    </div>
+    <div class="info__movie">
+      <div class="info__name">
+        <p class="name">Vote / Votes</p>
+        <p class="name">Popularity</p>
+        <p class="name">Original Title</p>
+        <p class="name">Genre</p>
+      </div>
+      <div class="info__value">
+        <p class="value">
+          <span class="value__orange">${
+            movie.vote_average
+          }</span> / <span class="value__white"
+            >${movie.vote_count}</span
+          >
+        </p>
+        <p class="value">${movie.popularity}</p>
+        <p class="value">${movie.original_title}</p>
+        <p class="value">${getGenre(genres)}</p>
+      </div>
+    </div>
+    <h2 class="about">ABOUT</h2>
+    <p class="about__text">${movie.overview}</p>
+    <ul class="button">
+      <li class="button__item">
+        <button class="button__watched" type="button">ADD TO WATCHED</button>
+      </li>
+      <li class="button__item">
+        <button class="button__queue" type="button">ADD TO QUEUE</button>
+      </li>
+    </ul>
+  </div>
+</div>
+  <div class="trailer__modal trailerHiden ">Trailer<div class="trailer">
+  <iframe class="iframe" id="iframe" frameborder="0" border="0" cellspacing="0" allowfullscreen src="" title="Youtube">
+  </iframe>
+  <button class="button_trailer-close">X</button></div></div>
+  `;
   const modalCloseBtn = document.querySelector('.modal__closeBtn');
   const traileModal = document.querySelector('.trailer__modal');
   const buttonTrailerClose = document.querySelector('.button_trailer-close');
@@ -133,7 +186,31 @@ async function showResultsOnSearch(e) {
     showGallery();
     return Notiflix.Notify.failure('Oops, there is no movie with that name');
   }
-  mainGallery.innerHTML = markup(data);
+  mainGallery.innerHTML = data
+    .map(movie => {
+      let date = '';
+      if (movie.release_date) {
+        date = movie.release_date;
+      } else if (movie.first_air_date) {
+        date = movie.first_air_date;
+      } else {
+        date = `No date`;
+      }
+      const genre = getGenre(movie.genre_ids);
+      const poster = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+        : noimage;
+      return `
+    <li class="card tracking" data-movie="${movie.id}">
+      <div class="movie__info">        
+        <img class="movie__image" src="https://image.tmdb.org/t/p/w500/${poster}" alt="movie-title" loading="lazy"/>
+      </div>
+    <p class="movie__name">${movie.title}</p>
+    <p class="movie__description"> ${genre}
+     | ${date.slice(0, 4)}</p>
+  </li>`;
+    })
+    .join('');
   const cards = document.querySelectorAll('.card');
   addEventToCard(cards);
 }
